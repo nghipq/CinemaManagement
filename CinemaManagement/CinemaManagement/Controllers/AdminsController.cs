a775111cce49eac4c5b31e53a351140ffbf54245
nghipq <nghipqce140179@fpt.edu.vn>
2020-10-02 15:12:24 +0700

    add input producer form

--- CinemaManagement/CinemaManagement/Controllers/AdminsController.cs
+++ CinemaManagement/CinemaManagement/Controllers/AdminsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using CinemaManagement.DAO;
 
 namespace CinemaManagement.Controllers
 {
@@ -11,6 +13,26 @@ namespace CinemaManagement.Controllers
         // GET: Admins
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: insertProducers
+        [HttpGet]
+        public ActionResult insertProducer()
+        {
+            return View();
+        }
+
+        // Post: insertProducers
+        [HttpPost]
+        public ActionResult insertProducer(FormCollection formCollection)
+        {
+            foreach(string key in formCollection.AllKeys)
+            {
+                Response.Write("Key = " + key + " ");
+                Response.Write(formCollection[key] + "</br>");
+            }
+
             return View();
         }
     }
