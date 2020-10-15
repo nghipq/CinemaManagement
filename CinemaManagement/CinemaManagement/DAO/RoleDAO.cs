7f17c73d973043b2d0abe3f040ededcb80a57533
Quoc-Toan <anlenki1240@gmail.com>
2020-10-15 17:09:35 +0700

    layout Per

---
+++ CinemaManagement/CinemaManagement/DAO/RoleDAO.cs
@@ -0,0 +1,51 @@
+﻿using CinemaManagement.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaManagement.DAO
+{
+    public class RoleDAO
+    {
+        private MySqlConnection conn { get; set; }
+
+        public RoleDAO()
+        {
+            this.conn = new DBConnection.DBConnection().conn;
+        }
+
+        public List<Role> getAllRole()
+        {
+            List<Role> Rlist = new List<Role>();
+
+            using (conn)
+            {
+                string sql = "select * from Role";
+                MySqlCommand com = new MySqlCommand(sql);
+                com.Connection = conn;
+
+                conn.Open();
+
+                MySqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Rlist.Add(new Role
+                    {
+                        id_Role = Convert.ToInt32(dr["id_Role"]),
+                        Role_Name = dr["Role_Name"].ToString(),
+                        Description = dr["Description"].ToString(),
+                        Status = Convert.ToBoolean(dr["status"])
+                    });
+                }
+
+                //đóng db sau khi dùng xong nhe
+                conn.Close();
+            }
+
+            return Rlist;
+        }
+    }
+}
